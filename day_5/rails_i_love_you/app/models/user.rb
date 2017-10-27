2c430aecc7b285d22979ce06ec0ded366648d98e
Halvard75 <metivier.anto@gmail.com>
2017-10-27 13:29:40 +0200

    sauvegarde rails_i_live_you

---
+++ day_5/rails_i_love_you/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+    
+    #pour que l'user puisse rédiger plusieurs posts
+    has_many :posts
+end
---
+++ day_5/rails_i_love_you/config/routes.rb
@@ -0,0 +1,19 @@
+Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/create'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+    devise_for :users
+    resources :posts
+    root to: 'home#index'
+end
